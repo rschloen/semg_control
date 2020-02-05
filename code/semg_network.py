e700d145122ca15147084a73c3baf96456cc7628
Robert Schloen <rschloen@gmail.com>
2020-02-05 13:58:55 -0600

    Added data and prepared it to pass through Network
    Full data signal is windowed and split into training/validation sets.
    Added a flatten before the fully connected layer, fixed error with
    dimension mismatch.

--- code/semg_network.py
+++ code/semg_network.py
@@ -8,12 +8,17 @@ import torch.optim as optim
 from torch.optim import lr_scheduler
 import torch.nn.functional as F
 import numpy as np
-import torchvision
-from torchvision import datasets, models, transforms
+# import torchvision
+# from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
 import time
 import os
 import copy
+from scipy.io import loadmat
+from statistics import mode
+# import scipy
+# import scipy.stats
+# print(scipy.__version__)
 
 plt.ion()   # interactive mode
 
@@ -23,20 +28,24 @@ class Network(nn.Module):
         super(Network,self).__init__()
         self.conv1 = nn.Conv2d(1,32,3)
         self.fc1 = nn.Linear(3072,12)
-        self.pool = nn.MaxPool2d((1,3))
+        self.pool = nn.MaxPool2d((3,1))
         self.relu = nn.ReLU()
 
     def forward(self,x):
         #convlution 1 to 32 feature maps, 3x3 kernel
         x = self.conv1(x)
+        # print('conv2d '+str(x.shape))
         #relu
         x = self.relu(x)
+        # print(x.shape)
         #max pooling 3x1
         x = self.pool(x)
+        x = torch.flatten(x,1)
         #fully connected layer
         x = self.fc1(x)
+        # print(x.shape)
         #softmax
-        return F.softmax(x)
+        return F.log_softmax(x)
 
 class Network_enhanced(nn.Module):
     def __init__(self):
@@ -84,15 +93,17 @@ class Network_enhanced(nn.Module):
         return F.softmax(x)
 
 
-def train_model(model,criterion,optimizer,scheduler,num_epochs=10):
-    """Modified example from pytorch tutorials"""
+def train_model(model,criterion,optimizer,scheduler,data,num_epochs=10):
+    """Modified example from pytorch tutorials, Author: Sasank Chilamkurthy"""
     since = time.time()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch,num_epochs-1))
         print('-'*10)
-        for phase in ['train','val']:
+        for phase in ['train','eval']:
             if phase == 'train':
                 model.train()
             else:
@@ -100,13 +111,18 @@ def train_model(model,criterion,optimizer,scheduler,num_epochs=10):
         running_loss = 0.0
         running_correct = 0
 
-        for input, labels in data[phase]:
+        for inputs, labels in data[phase]:
+            inputs = torch.from_numpy(inputs)
+            labels = torch.from_numpy(np.array(labels))
+            # print(inputs.shape)
+            inputs = inputs.view(1,1,inputs.shape[0],inputs.shape[1])
             inputs = inputs.to(device)
             labels = labels.to(device)
             optimizer.zero_grad()
 
             with torch.set_grad_enabled(phase == 'train'):
                 outputs = model(inputs)
+                print(outputs)
                 _, prediction = torch.max(outputs,1)
                 loss = criterion(outputs,labels)
 
@@ -115,7 +131,7 @@ def train_model(model,criterion,optimizer,scheduler,num_epochs=10):
                     optimizer.step()
 
                 running_loss += loss.item() * inputs.size(0)
-                running_correct += torch.sum(prediction = labels.data)
+                running_correct += torch.sum(prediction == labels.data)
             if phase == 'train':
                 scheduler.step()
             epoch_loss = running_loss/len(data[phase])
@@ -132,12 +148,40 @@ def train_model(model,criterion,optimizer,scheduler,num_epochs=10):
 
 
 def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = Network()
-    learning_rate = 1e-4
+    print(model)
+    learning_rate = 0.1
     optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
     criterion = torch.nn.MSELoss(reduction='sum')
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer,step_size=7,gamma=0.1)
 
+    x = loadmat('/home/rschloen/WinterProj/ninapro_data/s1/S1_E1_A1.mat') #Exercise 1. 12 hand gestures included gestures of interest
+    emg_data = x['emg'] #first 8 columns are from myo closest to elbow, next 8 are from second myo rotated 22 degs
+    restim = x['restimulus'] #restimulus and rerepetition are the corrected indexes for the movements
+    rep = x['rerepetition'] #6 repetitions per gesture
+
+    #Want a 260ms window (52 samples) of all eight channels as inputs with 235ms overlap (from paper)
+    window_size = 52 #samples, 260ms
+    overlap = 47 #sample, 235ms
+    step = window_size - overlap
+    i = 0
+    data = {'train':[],'eval':[]}
+    while i < len(emg_data[:200])-window_size:
+        if np.random.randint(1,11) < 8:
+            set = 'train'
+        else:
+            set = 'eval'
+
+        data[set].append([emg_data[i:i+window_size,:8],mode(list(restim[i:i+window_size][0]))])
+        i += step
+
+    # for input, label in data['train']:
+        # print(np.array([np.array([input])]).shape)
+
+    # input = torch.randn(20, 16, 50)
+    # print(input.shape)
+    best_model = train_model(model,criterion,optimizer,exp_lr_scheduler,data)
 
 
 
