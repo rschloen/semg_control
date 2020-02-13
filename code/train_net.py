5f2a1cceed0dec00441afb4d4f2c556adc575792
Robert Schloen <rschloen@gmail.com>
2020-02-13 14:09:43 -0600

    Wrote separate function for training/test
    train function loops through epochs, test function does one epoch
    both print progress/summary of loss and accuracy

--- code/train_net.py
+++ code/train_net.py
@@ -11,6 +11,7 @@ class Trainer():
         self.scheduler = scheduler
         self.device = device
         self.max_epochs = epochs
+        self.data_len = 1
 
         def one_epoch(phase):
             running_loss = 0.0
@@ -40,24 +41,39 @@ class Trainer():
             return running_loss, cor_classify
 
 
-        def train():
+        def train(mode=['train','eval']):
             since = time.time()
-            if phase == 'train':
-                self.model.train()
-                torch.set_grad_enabled(True)
-            elif phase == 'eval':
-                self.model.eval()
-                torch.set_grad_enabled(False)
-
+            self.model.train()
+            torch.set_grad_enabled(True)
             best_loss = float('+inf')
-            best_model_wts = copy.deepcopy(model.state_dict())
+            best_model_wts = copy.deepcopy(self.model.state_dict())
+            prev_loss = 0
+            loss_cnt = 0
 
-
+            print('Training...')
             for epoch in range(1,self.max_epochs+1):
                 e_loss, e_classify = one_epoch(phase)
+                e_acc = (e_classify.double()/len(data[phase]))*100 #percent
+                print("\tEpoch: {}/{}   Loss: {:.8f}    Accuracy: {:.4f}".format(epoch,self.max_epochs,e_loss,e_acc))
 
-                if e_loss <
+                if e_loss < best_loss:
+                    best_loss = e_loss
+                    best_model_wts = copy.deepcopy(self.model.state_dict())
+                '''Add early stopping: if change in loss less than ... x times, stop.
+                Useful check if updating properly as well'''
+                if (e_loss - prev_loss) < 1e-3: loss_cnt += 1
+                if loss_cnt > 2: break
+                prev_loss = e_loss
 
+        def test():
+            self.model.eval()
+            torch.set_grad_enabled(False)
+            test_loss, test_correct = one_epoch('test')
+            test_acc = (test_correct.double()/self.data_len)*100 #NEED DATA LENGTH
+            print("Test Summary:")
+            print("\tLoss = {:.8f}".format{test_loss})
+            print("\tCorrect: {}/{}".format{test_correct,self.data_len})
+            print("\tAccuracy: {:.4f}".format{test_acc})
 
 
 def main():
