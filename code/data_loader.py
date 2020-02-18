61e2cb63820fd575ab5b515182736803a7dbc056
Robert Schloen <rschloen@gmail.com>
2020-02-17 20:59:55 -0600

    Added 7th class and fixed dataset split
    Added fist gesture from each subject to the data set.
    
    Made the split between training, validation, and testing subsets
    of data. Testing subset is only to be used on fully trained model.
    Writing info about number of samples for each class that are included in
     each subset to txt file.
    
    Added the extension .npy hard coded in the class initializtion so it
    should not be in the path passed to create the class object so the path
    variable can be reused.

--- code/data_loader.py
+++ code/data_loader.py
@@ -4,11 +4,13 @@ from __future__ import print_function
 import torch
 from torch.utils import data
 import numpy as np
+from scipy.io import loadmat
+from statistics import mode
 
 
 class SEMG_Dataset(data.Dataset):
     def __init__(self,path,mode):
-        all_data = np.load(path,allow_pickle='TRUE').item()
+        all_data = np.load(path+'.npy',allow_pickle='TRUE').item()
         self.data = all_data[mode]
 
     def __len__(self):
@@ -25,20 +27,91 @@ class SEMG_Dataset(data.Dataset):
 
 
 if __name__ == '__main__':
-    path = "nina_data/all_6C_data_1.npy"
-    dataset = SEMG_Dataset(path,'eval')
-    params = {'batch_size': 4, 'shuffle': True,'num_workers': 4}
-    train_loader = data.DataLoader(dataset, **params)
-    count = 0
-    for input, output in train_loader:
-        print(input.size())
-        print(output.size())
-        count += 1
+    gen_data = False
+    if gen_data:
+        ## Load and combine data from all subjects
+        x = loadmat('../../ninapro_data/s1/S1_E1_A1.mat') #Exercise 1. 12 hand gestures included gestures of interest
+        emg_data = x['emg'] #first 8 columns are from myo closest to elbow, next 8 are from second myo rotated 22 degs
+        restim = x['restimulus'] #restimulus and rerepetition are the corrected indexes for the movements
+        rep = x['rerepetition'] #6 repetitions per gesture
+        for i in range(2,11):
+            x = loadmat('../../ninapro_data/s'+str(i)+'/S'+str(i)+'_E1_A1.mat') #Exercise 1. 12 hand gestures included gestures of interest
+            emg_data = np.vstack((emg_data,x['emg']))
+            restim = np.vstack((restim,x['restimulus']))
+            rep = np.vstack((rep,x['rerepetition']))
+        # print(restim[-100:])
+            y = loadmat('../../ninapro_data/s'+str(i)+'/S'+str(i)+'_E2_A1.mat')
+            e2 = y['emg']
+            e2_res = y['restimulus']
+            for n in range(len(e2)):
+                if e2_res[n] == 6:
+                    emg_data = np.vstack((emg_data,e2[n]))
+                    restim = np.vstack((restim,26))
 
-    print(count)
+        ## Initialize sampling parameters
+        ## Want a 260ms window (52 samples) of all eight channels as inputs with 235ms overlap (from paper)
+        window_size = 52 #samples, 260ms
+        overlap = 47 #sample, 235ms
+        step = window_size - overlap
+        i = 0
+        data = {'train':[],'val':[],'test':[]}
+        map = [(1,np.array([0,1,0,0,0,0,0])),(3,np.array([0,0,1,0,0,0,0])),(5,np.array([0,0,0,1,0,0,0])), (7,np.array([0,0,0,0,1,0,0])),(12,np.array([0,0,0,0,0,1,0])),(26,np.array([0,0,0,0,0,0,1]))]
+
+        ## Sort and label it for training/validation
+        while i < len(emg_data)-window_size:
+            # Split all data into either training(80%), validation(10%), or testing(10%)
+            if np.random.randint(1,11) < 9:
+                if np.random.randint(1,11) < 9:
+                    set = 'train'
+                else:
+                    set = 'val'
+            else:
+                set = 'test'
 
 
-    # data = np.load("nina_data/all_6C_data_1.npy",allow_pickle='TRUE').item()
-    # training_data = data['train']
-    # print(torch.from_numpy(training_data[0][0]))
-    # print(torch.from_numpy(training_data[0][1]))
+            label = mode(list(restim[i:i+window_size][0]))
+            ## Normalize Data??
+            emg_win = emg_data[i:i+window_size,:8]
+            normalize = True
+            if normalize:
+                emg_win = (emg_win - np.mean(emg_win))/(np.std(emg_win))
+                # emg_win = emg_win/128. #not helpful
+
+            if label == 0:
+                if np.random.randint(6) == 1: #only save about fifth of the rest states
+                    data[set].append([emg_win,np.array([1,0,0,0,0,0,0])])
+            else:
+                for act, new in map:
+                    if label == act:
+                        data[set].append([emg_win,new])
+            i += step
+
+        np.save("nina_data/all_7C_data_2.npy",data)
+
+    else:
+        ## Test distribution of
+        path = "nina_data/all_7C_data_1"
+        f = open(path+'.txt','w')
+        modes = ['train','val','test']
+        for phase in modes:
+            f.write("Data subset: "+phase+"\n")
+            dataset = SEMG_Dataset(path,phase)
+            params = {'batch_size': 10, 'shuffle': True,'num_workers': 4}
+            train_loader = data.DataLoader(dataset, **params)
+            test = [0,1,2,3,4,5,6]
+            # for c in test:
+            c1 = c2 = c3 = c4 = c5 = c6 = c7 = 0
+            for input, label in train_loader:
+                # print(torch.argmax(label))
+                c1 += (torch.argmax(label,dim=1) == test[0]).sum().item()
+                c2 += (torch.argmax(label,dim=1) == test[1]).sum().item()
+                c3 += (torch.argmax(label,dim=1) == test[2]).sum().item()
+                c4 += (torch.argmax(label,dim=1) == test[3]).sum().item()
+                c5 += (torch.argmax(label,dim=1) == test[4]).sum().item()
+                c6 += (torch.argmax(label,dim=1) == test[5]).sum().item()
+                c7 += (torch.argmax(label,dim=1) == test[6]).sum().item()
+            res = [c1,c2,c3,c4,c5,c6,c7]
+            for c in test:
+                print("Class {} has {} samples".format(c+1,res[c]))
+                f.write("Class {} has {} samples\n".format(c+1,res[c]))
+        f.close()
