d7d10b31b35f5f1addb36cda816875c624ed461c
Robert Schloen <rschloen@gmail.com>
2020-03-19 23:03:44 -0500

    Final comments/docstrings

--- code/semg_network.py
+++ code/semg_network.py
@@ -14,10 +14,9 @@ import os
 import copy
 
 
-# plt.ion()   # interactive mode
-
-
 class Network(nn.Module):
+    '''Basic Conv net architecture adapted from https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6111443/ (see figure 3).
+    Used primarily as an intro to pytorch'''
     def __init__(self,num_classes):
         super(Network,self).__init__()
         self.conv1 = nn.Conv2d(1,32,3)
@@ -28,26 +27,24 @@ class Network(nn.Module):
     def forward(self,x):
         #convlution 1 to 32 feature maps, 3x3 kernel
         x = self.conv1(x)
-        # print('conv2d '+str(x.shape))
         #relu
         x = self.relu(x)
-        # print(x.shape)
         #max pooling 3x1
         x = self.pool(x)
         x = torch.flatten(x,1)
         #fully connected layer
         x = self.fc1(x)
-        # print(x)
         #softmax
         return F.softmax(x,dim=1) #dim=1 refers to along the rows
 
 class Network_enhanced(nn.Module):
+    '''Enhanced Conv net architecture adapted from https://arxiv.org/pdf/1801.07756.pdf for raw emg. Used for much of the earlier
+    model training trials throughout this project'''
     def __init__(self,num_classes):
         super(Network_enhanced,self).__init__()
-        self.conv1_1 = nn.Conv2d(1,32,(5,3))#,padding=(4,2),stride=2)
-        self.conv2_1 = nn.Conv2d(32,64,(5,3))#,padding=(4,2),stride=2) #changed kernel from 5,3
-        # self.conv3 = nn.Conv2d(64,128,(5,3),padding=(4,2),stride=2)
-        self.fc1 = nn.Linear(1024,512) #changed 768 from 1024
+        self.conv1_1 = nn.Conv2d(1,32,(5,3))
+        self.conv2_1 = nn.Conv2d(32,64,(5,3))
+        self.fc1 = nn.Linear(1024,512)
         self.fc2 = nn.Linear(512,128)
         self.fc3 = nn.Linear(128,num_classes)
         self.pool = nn.MaxPool2d((3,1))
@@ -65,27 +62,16 @@ class Network_enhanced(nn.Module):
         #prelu
         x = self.prelu(x)
         #max pooling 3x1
-        # print(x.shape)
         x = self.pool(x)
-        # print(x.shape)
         x = self.conv2_1(x)
-        # print(x.shape)
-
         #batch normalization
         x = self.BN2(x)
         #prelu
         x = self.prelu(x)
+        #dropout
         x = self.drop(x)
         #max pooling 3x1
         x = self.pool(x)
-        # x = self.conv3(x)
-        # #batch normalization
-        # x = self.BN3(x)
-        # #prelu
-        # x = self.prelu(x)
-        # # x = self.drop(x)
-        #
-        # x = self.pool(x)
 
         x = torch.flatten(x,1)
         #fully connected layers
@@ -102,12 +88,13 @@ class Network_enhanced(nn.Module):
 
 
 class Network_XL(nn.Module):
+    '''Further expantion of the above Conv net architectures to improve pre-trained model's accuracy'''
     def __init__(self,num_classes):
         super(Network_XL,self).__init__()
-        self.conv1_1 = nn.Conv2d(1,16,(5,3),padding=(4,2))#,stride=2)
-        self.conv2_1 = nn.Conv2d(16,32,(5,3),padding=(4,2))#,stride=2) #changed kernel from 5,3
-        self.conv3 = nn.Conv2d(32,64,(5,3),padding=(4,2))#,stride=2)
-        self.fc1 = nn.Linear(2688,512) #changed 768 from 1024
+        self.conv1_1 = nn.Conv2d(1,16,(5,3),padding=(4,2))
+        self.conv2_1 = nn.Conv2d(16,32,(5,3),padding=(4,2))
+        self.conv3 = nn.Conv2d(32,64,(5,3),padding=(4,2))
+        self.fc1 = nn.Linear(2688,512)
         self.fc2 = nn.Linear(512,128)
         self.fc3 = nn.Linear(128,num_classes)
         self.pool = nn.MaxPool2d((3,1))
@@ -119,30 +106,20 @@ class Network_XL(nn.Module):
 
     def forward(self,x):
         #convlution 1 to 32 feature maps, 3x5 kernel
+        #batch normalization
+        #prelu
         x = self.prelu(self.BN1(self.conv1_1(x)))
-        #batch normalization
-        #prelu
         #max pooling 3x1
-        # print(x.shape)
         x = self.pool(x)
-        # print(x.shape)
         x = self.prelu(self.BN2(self.conv2_1(x)))
-        # print(x.shape)
-        #batch normalization
-        #prelu
+        #droput
         x = self.drop(x)
         #max pooling 3x1
         x = self.pool(x)
-
         x = self.prelu(self.BN3(self.conv3(x)))
         # #batch normalization
-        # x = self.BN3(x)
-        # #prelu
-        # x = self.prelu(x)
         x = self.drop(x)
-        #
         x = self.pool(x)
-
         x = torch.flatten(x,1)
         #fully connected layers
         x = self.fc1(x)
@@ -152,6 +129,5 @@ class Network_XL(nn.Module):
         x = self.prelu(x)
         x = self.fc3(x)
         x = self.prelu(x)
-
         #softmax
         return F.softmax(x,dim=1)
