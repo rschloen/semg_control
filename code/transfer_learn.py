abf2b2f3364a8d8c18161e6598cccbb29b4cae37
Robert Schloen <rschloen@gmail.com>
2020-03-19 22:53:22 -0500

    Final comments/docstrings
    Cleaned up old comments

--- code/transfer_learn.py
+++ code/transfer_learn.py
@@ -14,17 +14,25 @@ from semg_network import Network_XL, Network_enhanced
 from data_loader import SEMG_Dataset,window_data
 from train_net import Trainer, best_model_params
 import argparse, sys, csv
-import pnn_train as pnn
+# import pnn_train as pnn
 
 
 
 def most_active(array):
+    '''Calculates IEMG (integrated EMG) for each channel of rectified emg signal in array and returns most active channel.
+    ARGS: array: Can be either (str) path to csv file containing emg data or numpy array of data.
+    RETURNS: (int) most active channel'''
     if type(array) == str:
         return np.argmax(np.trapz(np.abs(np.loadtxt(array,delimiter=',')),axis=0))
     else:
         return np.argmax(np.trapz(np.abs(array),axis=0))
 
 def circle_shift(array,start,goal):
+    '''Circle shifts array from start to goal. Used for calibrating channels from different subjects/sessions
+    ARGS: array: emg data array
+          start: Most active channel for dataset
+          goal: Channel to line the most active channel up to
+    RETURNS: shifted: emg data array with channels shifted'''
     shifted = np.zeros(array.shape)
     s = goal - start
     for i in range(8):
@@ -50,36 +58,26 @@ if __name__ == '__main__':
         emg_array = np.zeros((1,8))
         gesture_array = np.array([])
         count = 0
-        for file in os.listdir("myo_rec_data/"):
+        for file in os.listdir("myo_rec_data/"): #count number of emg data files in folder
             if file.endswith(".csv"):
                 count += .5
-        # print(int(count))
         count -= 1
         first_activity =  most_active('myo_rec_data/raw_emg_'+path+'1.csv')
-        # first_activity = most_active('nina_data/combined_emg_data.csv')
         print('Target channel {}'.format(first_activity))
 
-        count = 1
-        for i in range(int(count)):
-            # print(first_activity)
+        for i in range(int(count)): # loop through data files in folder to build dataset containing all of them
             emg_array = np.zeros((1,8))
             gesture_array = np.array([])
-            # temp_arr = np.loadtxt('myo_rec_data/raw_emg_'+path+'{}.csv'.format(i+1),delimiter=',')
-            temp_arr = np.loadtxt('myo_rec_data/raw_emg_JRS_7C_testing.csv',delimiter=',')
+            temp_arr = np.loadtxt('myo_rec_data/raw_emg_'+path+'{}.csv'.format(i+1),delimiter=',')
             active_chan = most_active(temp_arr)
-            # print('File {} most active channel {}'.format(i,active_chan))
-            # print(temp_arr[:10])
+            print('File {} most active channel {}'.format(i,active_chan))
             shifted_array = circle_shift(temp_arr,active_chan,first_activity)
-            # print(shifted_array[:10])
             emg_array = np.vstack((emg_array,shifted_array))
             gesture_array = np.append(gesture_array,np.loadtxt('myo_rec_data/gesture_'+path+'{}.csv'.format(i+1),delimiter=','))
 
-
-        emg_array = np.delete(emg_array,0,0)
-        # emg_array = np.loadtxt('myo_rec_data/raw_emg_JRS_7C_testing.csv',delimiter=',')
-        # gesture_array = np.loadtxt('myo_rec_data/gesture_JRS_7C_testing.csv',delimiter=',')
-        new_path = 'myo_rec_data/win_JRS_7C_testing_shifted'.format(i)
-        with open('myo_rec_data/win_JRS_7C_testing_shifted_stats.txt','w') as real_time:#, open('real_time_gest.csv','w') as real_gest:
+        emg_array = np.delete(emg_array,0,0) #first row was initialized with zeros, needs to be removed
+        new_path = 'myo_rec_data/win_JRS_7C_comb7_shifted'.format(i) #file to store new combined dataset
+        with open('myo_rec_data/win_JRS_7C_comb7_shifted_stats.txt','w') as real_time:
             stat_writer = csv.writer(real_time, delimiter=',')
             mean_emg = np.mean(emg_array)
             std_emg = np.std(emg_array)
@@ -88,48 +86,51 @@ if __name__ == '__main__':
         window_data(emg_array,gesture_array.T.astype(int),new_path,label_map)
 
     else:
-        WT_PATH = 'nina_data/all_7C_data_comb_XL_cross'
-        PATH = 'nina_data/all_7C_data_comb'
-        # new_PATH = 'myo_rec_data/win_JRS_7C_comb7_shifted'
-        # model = Network_enhanced(7)
+        WT_PATH = 'nina_data/all_7C_data_comb_XL_cross' # file with model state dictionary
+        PATH = 'nina_data/all_7C_data_comb' # file with pretraining data
+        new_PATH = 'myo_rec_data/win_JRS_7C_comb7_shifted' # file with new data for transfer learning
         model = Network_XL(7)
         device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
-        # nt = best_model_params(model,PATH)
-        # nt.stats['val']['model_wt'] = torch.load(WT_PATH+'.pt',map_location='cpu')
-        # tl,ta = nt.test(use_best_wt=True,epoch=1)
 
-        # num2 = model.fc2.in_features
-        # num3 = model.fc3.in_features
+        # Test pretrained network
+        nt = best_model_params(model,PATH)
+        nt.stats['val']['model_wt'] = torch.load(WT_PATH+'.pt',map_location='cpu')
+        tl,ta = nt.test(use_best_wt=True,epoch=1)
+
         model.load_state_dict(torch.load(WT_PATH+'.pt',map_location='cpu'))
-        for param in model.parameters():
-            param.requires_grad = False
-        # # for param in model.conv3.parameters():
-        # #     param.requires_grad = True
-        # # for param in model.fc1.parameters():
+
+        ## Examples of selectively freezing or unfreezeing layer's weights for transfer learning
+        # num2 = model.fc2.in_features
+        # model.fc2 = nn.Linear(num2, num3)
+        # for param in model.parameters():
+        #     param.requires_grad = False
+        # for param in model.conv3.parameters():
+        #     param.requires_grad = True
+        # for param in model.fc1.parameters():
         # #     param.requires_grad = True
         # # for param in model.fc2.parameters():
         # #     param.requires_grad = True
-        # # for param in model.fc3.parameters():
-        # #     param.requires_grad = True
-        # model.fc2 = nn.Linear(num2, num3)
+        # for param in model.fc3.parameters():
+        #     param.requires_grad = True
+        # num3 = model.fc3.in_features
         # model.fc3 = nn.Linear(num3, 7)
         # model = ft_nt.model.to(device)
 
-        # ft_nt.max_epochs = 500
-        # ft_nt.early_stop = True
+        # Begin transfer learning
+        ft_nt = best_model_params(model,new_PATH)
+        ft_nt.max_epochs = 500
+        ft_nt.early_stop = True
+        ft_nt.train(val_train=True)
+        tl, ta = ft_nt.test(use_best_wt=True, epoch=1)
+        torch.save(ft_nt.wt_hist['val'][np.argmin(ft_nt.loss_hist['val'])],new_PATH+'_XL_cross_tran_final2.pt')
+        plot_path = 'win_JRS_7C_comb7_shifted_XL_cross_tran_final'
+        ft_nt.plot_loss(plot_path,True)
+
+        '''For PNN (incomplete)'''
+        # dir = 'myo_rec_data/win_JRS_7C_'
+        # new_PATH = [dir+'1_shifted',dir+'2_shifted',dir+'3_shifted',dir+'4_shifted',dir+'5_shifted',dir+'6_shifted']
         # ft_nt = pnn.best_model_params(model,new_PATH)
-        # #
-        # ft_nt.train(val_train=True)
-        # tl, ta = ft_nt.test(use_best_wt=True, epoch=1)
-        # torch.save(ft_nt.wt_hist['val'][np.argmin(ft_nt.loss_hist['val'])],new_PATH+'_XL_cross_tran2.pt')
-        # plot_path = 'win_JRS_7C_comb7_shifted_XL_cross_tran2'
-        # ft_nt.plot_loss(plot_path,True)
-
-        dir = 'myo_rec_data/win_JRS_7C_'
-        new_PATH = [dir+'1_shifted',dir+'2_shifted',dir+'3_shifted',dir+'4_shifted',dir+'5_shifted',dir+'6_shifted']
-        ft_nt = pnn.best_model_params(model,new_PATH)
-        print(len(ft_nt.new_model.parameters()))
-
+        # print(ft_nt.new_model.state_dict())
         # ft_nt.train(val_train=True)
         # tl, ta = ft_nt.test(use_best_wt=True, epoch=1)
         # torch.save(ft_nt.wt_hist['val'][np.argmin(ft_nt.loss_hist['val'])],new_PATH+'_XL_cross_tran2.pt')
